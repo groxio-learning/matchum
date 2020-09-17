0eb584d562a8017cb76af27767a3ae5e2d30819d
mohammed hindawash <djslakivich@gmail.com>
2020-09-17 19:25:14 -0400

    Added a build_game function, and expanded the mount as well as added some inspects for the new atoms

--- lib/matchum_web/live/game_live.ex
+++ lib/matchum_web/live/game_live.ex
@@ -1,14 +1,26 @@
 defmodule MatchumWeb.GameLive do
   use MatchumWeb, :live_view
+  alias Matchum.Game
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, message: "hello")}
+    {
+      :ok,
+      assign(socket, board: Game.new())
+      |> build_game()
+    }
+  end
+
+  def build_game(socket) do
+    assign(socket, game: Game.board_to_map(socket.assigns.board))
   end
 
   def render(assigns) do
     ~L"""
-    <h1><%= @message %></h1>
+    <pre>
+    <%= inspect @board %>
+    <%= inspect @game %>
+    </pre>
     """
   end
 end
